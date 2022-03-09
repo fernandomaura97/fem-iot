781f4d4acaf786725c3f25f4639b1b2e9f3529ea
PradeepKumar <fernandomaurarivero@gmail.com>
2022-03-09 14:41:49 +0100

    association v1.0: working on cooja

--- fem-iot/1-3_association/coord.c
+++ fem-iot/1-3_association/coord.c
@@ -18,6 +18,8 @@
 static linkaddr_t addr_stas[ROUTENUMBER]; //store sta's addresses in here, for routing and sending
 static linkaddr_t buffer_addr; 
 
+const linkaddr_t addr_empty = {{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}; //placeholder address
+
 const uint8_t beacon_[3] = {0x05, 0x06, 0x07}; 
 const uint8_t ass_req[3] = {0x08, 0x09, 0x0A};
 const uint8_t ass_response[3] = {0x0B, 0x0C, 0x0D};
@@ -35,30 +37,15 @@ void input_callback(const void *data, uint16_t len,
 {
   printf("Callback received\n");
  
-  uint8_t i;
-  uint8_t oldaddr = 0; 
+  
   //int pos = 0;
 
   linkaddr_copy	(&buffer_addr, src ); //copy the address of the packet received into the buffer
 
 
-  for (i= 0; i<ROUTENUMBER; i++){
-      //if (addr_stas[i] == *src){
-    if (linkaddr_cmp(&addr_stas[i], src)){ //if they are the same then
-      oldaddr = 1;
-      printf("Address already found at pos %d\n", i);
-      break;     
-    }   //if we have this address in our list, we don't need to add it again
-    else{
-        if(oldaddr ==0){
-          linkaddr_copy(&addr_stas[i], src); //if we don't have it, add it
-          printf("Address added at pos %d\n", i);
-          oldaddr = 1;
-          break;
-        }
-    }
+  
         
-  }
+  
   /*if(!oldaddr){
     printf("New address: ");
     LOG_INFO_LLADDR(src);
@@ -66,7 +53,7 @@ void input_callback(const void *data, uint16_t len,
     addr_stas[pos] = *src;
   }
   */
-  if((memcmp(data, ass_req, 3) == 0)&&!oldaddr)
+  if((memcmp(data, ass_req, 3) == 0))
   {
     printf("Assoc. request received");
     
@@ -120,6 +107,38 @@ PROCESS_BEGIN()
 while(1)
 {
   PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+  uint8_t i;
+  uint8_t oldaddr = 0; 
+  for (i= 0; i<ROUTENUMBER; i++){
+      //if (addr_stas[i] == *buffer_addr){
+    if (linkaddr_cmp(&addr_stas[i], &buffer_addr)){ //if they are the same then
+      oldaddr = 1;
+      printf("Address already found at pos %d\n", i);
+      break;     
+    }   //if we have this address in our list, we don't need to add it again
+    
+        
+  }
+
+  if(!oldaddr){
+    for(i = 0; i<ROUTENUMBER; i++){
+       if(linkaddr_cmp(&addr_stas[i], &addr_empty))
+        {
+          linkaddr_copy(&addr_stas[i], &buffer_addr); //if we don't have it, add it
+          printf("Address");
+          LOG_INFO_LLADDR(&buffer_addr);
+          printf(" added at pos %d\n", i);
+          break;
+          //oldaddr = 1;     
+        }
+    }
+}
+
+
+
+
+
+
   //nullnet_set_input_callback(NULL);
   LOG_INFO("Associating nowwwwww\n");
   uint8_t ibuf[3] = {0x0B, 0x0C, 0x0D};
