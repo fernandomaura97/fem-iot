3905a39ccf395d65d7bb21c37e9a4f2a29dd2207
PradeepKumar <fernandomaurarivero@gmail.com>
2022-05-11 16:29:39 +0200

    dualband gw->868 working, need to define uart_aggregation

--- fem-iot/10-5 _DualBand_HARE/db_868/dualband_868.c
+++ fem-iot/10-5 _DualBand_HARE/db_868/dualband_868.c
@@ -11,9 +11,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+#include "random.h"
 
 /*---------------------------------------------------------------------------*/
 
+
+
+#define NODEID1 1  // non-adaptive, test
+#define NODEID2 2 //DHT22
+#define NODEID3 4 // PM10 
+#define NODEID4 8
+#define NODEID5 16
+#define NODEID6 32
+#define NODEID7 64
+#define NODEID8 128
+
+#define NODEID NODEID2
+
+
+#define T_MDB  (10 * CLOCK_SECOND)
+#define T_SLOT  (1.5 * CLOCK_SECOND)
+#define T_GUARD  (0.5 * CLOCK_SECOND)
+#define T_BEACON (60 * CLOCK_SECOND)
 
 #define LOG_MODULE "DB_868"
 #define LOG_LEVEL LOG_LEVEL_DBG
@@ -33,17 +52,57 @@ static uint8_t buffer[7];
 const char delimitador[3] = " ";
 long int sortida[3];
 char* endPtr;
+static uint8_t nodeid;
+volatile static uint8_t bitmask; 
+
+static clock_time_t time_of_beacon_rx; // time of beacon reception
+
+static bool bcon_flag = 0; // flag que indica si el node ha rebut cap beacon
+
+
+static struct etimer next_beacon_etimer;
+
+const uint8_t power_levels[3] = {0x46, 0x71, 0x7F}; // 0dB, 8dB, 14dB
 
 volatile static uint16_t len_msg;
-static bool is_associated = 0; // flag to check if the node is associated
+static volatile  bool is_associated = 0; // flag to check if the node is associated
 
 static linkaddr_t from, gw_addr; 
 //static bool flag = 0;  
+
+uint8_t am_i_polled(uint8_t bitmask, uint8_t id)  //prints which nodes the beacon is polling, and if it's own NODEID return position of poll
+{
+    uint8_t masked = bitmask & id;
+    if (masked & id){
+        return 1;
+    }
+    else{
+        return 0; 
+    }         
+}
+//function to print the number of the NODEID (f.e. NODEID8 would return 8)
+uint8_t get_nodeid(uint8_t id)
+{
+    uint8_t i;
+    for (i = 0; i < 8; i++)
+    {
+        if (id & (1 << i))
+        {
+            return (i+1);
+        }
+    }
+    return 0;
+}
+
+
+
 /*---------------------------------------------------------------------------*/
 PROCESS(dualband_868, "dualband 868");
 PROCESS(radio_rx_process, "rx process");
 PROCESS(associator_process, "assoc process");
-AUTOSTART_PROCESSES(&dualband_868, &radio_rx_process);
+PROCESS(poll_process, "poll process");
+
+AUTOSTART_PROCESSES(&dualband_868, &radio_rx_process, &poll_process, &associator_process);
 /*---------------------------------------------------------------------------*/
 
 unsigned int uart1_send_bytes(const unsigned char *s, unsigned int len){  
@@ -136,7 +195,8 @@ PROCESS_THREAD(dualband_868, ev, data){
  
   PROCESS_BEGIN();
   nullnet_set_input_callback(input_callback);
- 
+  nodeid = get_nodeid(NODEID);
+  printf("Nodeid: %d\n", nodeid);
   uart_set_input(1, print_uart);
   while(1){
         
@@ -157,10 +217,11 @@ PROCESS_THREAD(dualband_868, ev, data){
 PROCESS_THREAD(radio_rx_process, ev, data){
 
 static struct etimer beacon_no_timer;
-static bool bcon_flag = 0; 
-
 volatile static uint8_t* datapoint;
 PROCESS_BEGIN();
+
+
+
 
 while(1){
 
@@ -171,9 +232,10 @@ while(1){
 
 
   uint8_t frame_header = (datapoint[0] & 0b11100000) >> 5; 
+  bitmask = datapoint[1];
   
   if(frame_header == 0){
-    printf("beacon\n");
+    //printf("beacon\n");
     
     linkaddr_copy(&gw_addr, &from);
 
@@ -182,8 +244,8 @@ while(1){
 
     if(bcon_flag == 0){
 
-      bcon_flag = 1; 
-
+    
+     
       
       if(Beacon_no == 0){
         
@@ -192,6 +254,7 @@ while(1){
         LOG_DBG("Beacon 0 received\n");
 
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&beacon_no_timer));
+        process_poll(&associator_process);
       }
       else if(Beacon_no == 1)
       {   
@@ -199,11 +262,12 @@ while(1){
         LOG_DBG("Beacon 1 received\n");
         etimer_set(&beacon_no_timer, CLOCK_SECOND/2 );
         PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&beacon_no_timer));
-
+        process_poll(&associator_process);
       }
       else if(Beacon_no ==2)
       {   
         LOG_DBG("Beacon 2 received\n");
+        process_poll(&associator_process); 
       }
       // TODO: find a METHOD to wait for the 1.5 seconds after first beacon, or time depending on the message received first    
       else{
@@ -213,7 +277,7 @@ while(1){
 
      
 
-      printf("Beacon received: %d %d %d\n", datapoint[0], datapoint[1], datapoint[2]);
+      //printf("Beacon received: %d %d %d\n", datapoint[0], datapoint[1], datapoint[2]);
       
       char string[20];
 
@@ -221,11 +285,11 @@ while(1){
 
       uart1_send_bytes((uint8_t *)string, sizeof(string) - 1);
       printf("beacon sent UART\n");
-    }
+      } //if_beacon_flag
       
-  }
+  } //if_frame_header==0
   else if(frame_header == 1){
-    LOG_DBG("association request received\n");
+    LOG_DBG("association request received ??\n");
 
   }
 
@@ -235,9 +299,9 @@ while(1){
     if(linkaddr_cmp(&from, &gw_addr)) { 
 
           LOG_INFO("Not associated, associating now\n");
-          PROCESS_CONTEXT_BEGIN(&associator_process);
+          //PROCESS_CONTEXT_BEGIN(&associator_process);
           is_associated = true; 
-          PROCESS_CONTEXT_END(&associator_process);
+          //PROCESS_CONTEXT_END(&associator_process);
         }
         else{
             LOG_DBG("error, different adresses");
@@ -247,7 +311,19 @@ while(1){
 
   else if(frame_header == 3){
 
-    LOG_DBG("Sensor data received !? \n");
+    LOG_DBG("Poll message \n");
+    if(is_associated){
+      process_poll(&poll_process);
+    else{
+      printf("Not associated, ignoring poll\n");
+
+      /*
+      REASSOCIATION COULD BE DONE HERE
+      */
+
+
+
+    }
 
   }
 
@@ -258,18 +334,183 @@ while(1){
 }
 PROCESS_END();
 
-
-
 }
 
 
 PROCESS_THREAD(associator_process, ev, data){
+
+
+   
+  static clock_time_t time_to_wait; 
+  static uint8_t buf[2];
+  static struct etimer asotimer; 
+  static struct etimer radiotimer; 
+  static bool amipolled_f = 0;
+  static uint8_t i_pwr;
   PROCESS_BEGIN();
+
+
 
   while(1){
     PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
-    printf("associating\n");
 
+    time_of_beacon_rx = RTIMER_NOW();
+
+    time_to_wait = (T_MDB  + ((nodeid -1) * (T_SLOT + T_GUARD)))- T_GUARD;
+
+    printf("time to poll is  %lu\n", time_to_wait/CLOCK_SECOND);
+    etimer_set(&radiotimer, time_to_wait);
+
+
+    if(!is_associated){
+      LOG_INFO("associating\n");
+      i_pwr= 0;            
+         
+      buf[0] = 0b00100000; //association request
+      buf[1] = nodeid; 
+
+      nullnet_buf = (uint8_t *)buf;
+      nullnet_len = sizeof(buf);
+
+      for (i_pwr = 0; i_pwr < 3; i_pwr++){
+               
+
+        etimer_set(&asotimer, 2* CLOCK_SECOND + (random_rand() % (CLOCK_SECOND)));   //add some jitter/randomness for the transmission   
+        
+        NETSTACK_RADIO.set_value(RADIO_PARAM_TXPOWER, power_levels[i_pwr]);
+        
+        
+        PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&asotimer));
+        
+        if(is_associated == 1 ){
+       
+        break;}
+                                   
+        
+
+        printf("Sending assoc. Request, tx power: %02x\n", power_levels[i_pwr]);
+        //LOG_INFO_LLADDR(&gw_addr);
+        NETSTACK_NETWORK.output(&gw_addr);  
+
+            
+      } 
+     
+      if(!is_associated) 
+      {
+          printf("I'm STILL not associated!!\n");
+      }
+
+      else //if is associated
+        {
+            printf("I'm already associated\n");
+            printf("bitmask: %02x, nodeid: %d\n", bitmask, nodeid);
+        }
+      } //if_!is_associated
+
+
+      /*----------------------------------------------------------------------------------------*/
+      //SECOND PART, WAITING UNTIL POLL TIME
+
+
+      amipolled_f = am_i_polled(bitmask, nodeid);
+
+      if( amipolled_f == 1){
+          printf("I'm transmitting in the %dth slot\n", nodeid);
+          
+          //time_until_poll = T_MDB + ((NODEID-1) * (T_SLOT + T_GUARD)) - T_GUARD;
+          //printf("radio off, time until radio on: %lu ticks, %lu seconds\n", time_until_poll ,time_until_poll/CLOCK_SECOND);              
+          NETSTACK_RADIO.off();
+          RTIMER_BUSYWAIT(5);
+          //etimer_set( &radiotimer, time_until_poll);
+          PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&radiotimer));
+          
+          NETSTACK_RADIO.on();
+          printf("radio back on\n");
+      }
+      else if (amipolled_f == 0) { //if not polled, just wait for the next beacon
+          printf("Radio off until the next beacon\n");
+          NETSTACK_RADIO.off();
+          RTIMER_BUSYWAIT(5);
+          etimer_set( &radiotimer, T_BEACON - 2*CLOCK_SECOND);
+          PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&radiotimer));
+
+          NETSTACK_RADIO.on();
+          printf("radio back on, beacon in ~2s\n");
+      }   
+      amipolled_f = 0;   
   }
+  PROCESS_END();
+}
+
+PROCESS_THREAD(poll_process,ev,data)
+{
+
+  static clock_time_t time_after_poll; 
+
+
+  PROCESS_BEGIN();
+  PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+  printf("polling\n");
+
+  /*
+
+  //Routine to be executed when we get a poll for the sensors: 
+
+  //IF associated, data UART in is ok, send to gateway in the correct slot. 
+
+  */
+ while(1){
+  
+
+  PROCESS_WAIT_EVENT_UNTIL(ev == PROCESS_EVENT_POLL);
+
+
+  buffer_poll = packetbuf_dataptr();
+  
+  if(buffer_poll[1] == nodeid) //if the poll is for me
+  {
+
+
+
+
+    
+    //if(data_uart_in_ok())
+    //{
+      //send_data_to_gateway();
+  //}
+
+
+
+
+    printf("finished sending data\n");
+    
+
+
+    NETSTACK_RADIO.off();
+    RTIMER_BUSYWAIT(5);
+    time_after_poll = RTIMER_NOW() - time_of_beacon_rx;
+    printf("setting timer for %lu seconds. Time now: %lu, Time of beacon : %lu, dt : %lu\n", T_BEACON - 10*CLOCK_SECOND - time_after_poll/CLOCK_SECOND, RTIMER_NOW()/CLOCK_SECOND, time_of_beacon_rx/CLOCK_SECOND, time_after_poll/CLOCK_SECOND);
+    etimer_set(&next_beacon_etimer, T_BEACON - 3*CLOCK_SECOND - time_after_poll);
+    printf("still here\n");
+
+
+    PROCESS_WAIT_UNTIL(etimer_expired(&next_beacon_etimer));
+    NETSTACK_RADIO.on();
+    printf("radio back on, beacon in ~2s!!! \n");
+    
+
+    PROCESS_CONTEXT_BEGIN(&radio_rx_process);
+    bcon_flag = 0;
+    printf("bcon_flag == %d\n", bcon_flag);
+    PROCESS_CONTEXT_END(&radio_rx_process);
+  }
+  else{
+    printf("not my poll, %d\n", buffer_poll[1]);
+  }
+
+ }
+   
+
+
   PROCESS_END();
 }
